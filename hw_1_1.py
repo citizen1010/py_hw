4a0e482b81de009584dc23d23bc39b9b9f253d8e
citizen1010 <57265411+citizen1010@users.noreply.github.com>
2020-03-12 16:20:04 +0300

    Add files via upload

---
+++ hw_1_1.py
@@ -0,0 +1,48 @@
+"""
+Alisher Zharbossynov
+
+Permutation function
+"""
+from itertools import permutations
+
+
+def permute(n):
+    if n == 1:
+        print(n)
+    elif 2 <= n <= 7:
+        sorted_array = create_sort_array(n)
+        result_array = permute_array(sorted_array)
+        print_permutation_elements(result_array)
+    else:
+        print("Error: 1 <= n <= 7")
+
+
+def create_sort_array(n):
+    array = []
+    while n > 0:
+        array.append(n)
+        n = n - 1
+    array.sort()
+    return array
+
+
+def permute_array(sorted_array):
+    new_array = []
+    for i in permutations(sorted_array):
+        new_array.append(i)
+    new_array = list(map(list, new_array))  # converting list of tuples to list of lists
+    return new_array
+
+
+def print_permutation_elements(result_array):
+    for element in result_array:
+        print(' '.join(map(str, element)), end='\n')  # printing without brackets and commas
+
+
+print("Result for 'n' = 1:")
+permute(1)
+print("Result for 'n' > 7:")
+permute(9)
+print("Result for 'n' => 2  and 'n' <= 7:")
+permute(5)
+
---
+++ hw_1_2.py
@@ -0,0 +1,110 @@
+"""
+Alisher Zharbossynov
+
+BST: insert, delete, search, number of elements less than a given number
+"""
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, insert_value):  # добавление
+        if insert_value < self.value:
+            if self.left is None:
+                self.left = Node(insert_value)
+            else:
+                self.left.insert(insert_value)
+        else:
+            if self.right is None:
+                self.right = Node(insert_value)
+            else:
+                self.right.insert(insert_value)
+
+    def min_value_node_delete(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def delete(self, delete_value):  # удаление
+        if delete_value < self.value:
+            self.left = self.left.delete(delete_value)
+        elif delete_value > self.value:
+            self.right = self.right.delete(delete_value)
+        else:
+            if self.left is None:
+                temp = self.right
+                return temp
+            elif self.right is None:
+                temp = self.left
+                return temp
+            temp = self.min_value_node_delete(self.right)
+            self.value = temp.value
+            self.right = self.right.delete(temp.value)
+        return self
+
+    def search(self, search_value):  # поиск
+        if search_value == self.value:
+            return True
+        elif search_value < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.search(search_value)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(search_value)
+
+    def inorder_traversal(self, root):
+        res = []
+        if root:
+            res = self.inorder_traversal(root.left)
+            res.append(root.value)
+            res = res + self.inorder_traversal(root.right)
+        return res
+
+    def numbers_min_than(self, root, num):  # кол-во элементов в дереве меньше заданного
+        array = self.inorder_traversal(root)
+        result = 0
+        for x in array:
+            if num > x:
+                result += 1
+        print(result)
+
+
+def main():
+    node = Node(50)
+    node.insert(30)
+    node.insert(20)
+    node.insert(40)
+    node.insert(70)
+    node.insert(60)
+    node.insert(80)
+
+    print("Inorder: ---------")
+    print(node.inorder_traversal(node))
+    print("Number of elements less than a given number:")
+    node.numbers_min_than(node, 60)
+    print("")
+    print("Results of search:")
+    print(node.search(10))
+    print(node.search(90))
+    print(node.search(70))
+    print("")
+    node.delete(20)
+    node.delete(30)
+    node.delete(50)
+    print("Inorder after delete: ")
+    print(node.inorder_traversal(node))
+    print("Number of elements less than a given number:")
+    node.numbers_min_than(node, 60)
+
+
+if __name__ == "__main__":
+    main()
